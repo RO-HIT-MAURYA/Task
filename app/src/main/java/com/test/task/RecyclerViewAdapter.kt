abd56c792774ea81b3ce6eb323c1a304a727c239
RO-HIT-MAURYA <40297562+RO-HIT-MAURYA@users.noreply.github.com>
2020-07-07 21:26:55 +0530

    Color of progress bar is changed.

--- app/src/main/java/com/test/task/RecyclerViewAdapter.kt
+++ app/src/main/java/com/test/task/RecyclerViewAdapter.kt
@@ -1,9 +1,14 @@
 package com.test.task
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
@@ -46,29 +51,41 @@ class RecyclerViewAdapter(
 
         holder.titleTextView.text = jsonObject.get(CommonParameters.original_title).asString
         holder.bodyTextView.text = jsonObject.get(CommonParameters.overview).asString
-        holder.ratingTextView.text =
-            getPercent(jsonObject.get(CommonParameters.vote_average).asFloat).toString() + "%"
-        holder.progressBar.secondaryProgress =
-            getPercent(jsonObject.get(CommonParameters.vote_average).asFloat)
+
+        val percent = getPercent(jsonObject.get(CommonParameters.vote_average).asFloat)
+        holder.ratingTextView.text = "$percent%"
+
+        if (percent < 60)
+            holder.progressBar.secondaryProgressTintList =
+                ColorStateList.valueOf(Color.parseColor("#FF0000"))
+        else if (percent in 60..79)
+            holder.progressBar.secondaryProgressTintList =
+                ColorStateList.valueOf(Color.parseColor("#FFEB3B"))
+        else
+            holder.progressBar.secondaryProgressTintList =
+                ColorStateList.valueOf(Color.parseColor("#00FF00"))
+
+        holder.progressBar.secondaryProgress = percent
+
         holder.bodyTextView.text = jsonObject.get(CommonParameters.overview).asString
 
-        holder.dateTextView.text = changeDateFormat(jsonObject.get(CommonParameters.release_date).asString)
+        holder.dateTextView.text =
+            changeDateFormat(jsonObject.get(CommonParameters.release_date).asString)
 
         if (position == jsonList.size - 1)
             reloadCallBack.reload()
     }
 
     class RecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imageView = itemView.imageView
-        val titleTextView = itemView.titleTextView
-        val bodyTextView = itemView.bodyTextView
-        val progressBar = itemView.progressBar
-        val ratingTextView = itemView.ratingTextView
-        val dateTextView = itemView.dateTextView
+        val imageView: ImageView = itemView.imageView
+        val titleTextView: TextView = itemView.titleTextView
+        val bodyTextView: TextView = itemView.bodyTextView
+        val progressBar: ProgressBar = itemView.progressBar
+        val ratingTextView: TextView = itemView.ratingTextView
+        val dateTextView: TextView = itemView.dateTextView
     }
 
-    companion object
-    {
+    companion object {
         fun changeDateFormat(string: String): CharSequence {
             var simpleDateFormat = SimpleDateFormat("yyyy-MM-dd")
             val date = simpleDateFormat.parse(string)
